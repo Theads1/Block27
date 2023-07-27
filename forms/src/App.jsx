1f0400895c4521bfb1ed45faaadd7d99cd80fa8f
Theads1 <tiaradheads@gmail.com>
2023-07-27 12:33:22 -0400

    trial run

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -1,13 +1,16 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import Authenticate from "./components/authenticate";
+import SignUpForm from "./components/signupform";
 import './App.css'
 
 function App() {
-
+const [token, setToken]= useState(null)
   return (
     <>
-
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
     </>
-  )
+  );
 }
 
 export default App
